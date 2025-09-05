53964a3e2b3586739ce99b3a55ffa01e6795456f
Blue084-end <azend11110000@gmail.com>
2025-09-06 00:19:29 +0700

    Update Rua.py

--- Rua.py
+++ Rua.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn.linear_model as lm
-model = lm.LinearRegression()
+from sklearn.linear_model import LinearRegression
+
 
 
 
